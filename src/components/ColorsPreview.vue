b9e4f74d844fd7fe46cf1c35ebfab54883126f15
Thomas Günther <mail@medienbaecker.com>
2022-01-25 14:46:54 +0100

    Add field preview

---
+++ src/components/ColorsPreview.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: {
+        value: String
+    }
+}
+</script>
+
+<template>
+    <span class="k-colors-structure-preview" :style="{ backgroundColor: value }"></span>
+</template>
+
+<style>
+    .k-colors-structure-preview {
+        display: block;
+        width: 1.5rem;
+        height: 1.5rem;
+        margin: 0 .75rem;
+    }
+</style>
